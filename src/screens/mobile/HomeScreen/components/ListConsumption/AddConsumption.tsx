16e4a8320db7a6596d72c6fc0d57be98661b1f1d
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-07 13:50:33 -0300

    feat: add button add bottle consumption

--- src/screens/mobile/HomeScreen/components/ListConsumption/AddConsumption.tsx
+++ src/screens/mobile/HomeScreen/components/ListConsumption/AddConsumption.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, TouchableOpacity, View } from "react-native";
 import { format, isToday } from "date-fns";
 
 import { dbService } from "@db";
-import { Text } from "@components";
+import { Icon, Text } from "@components";
 import { usePreferencesContext } from "@hooks";
 
 interface Props {
@@ -12,11 +12,12 @@ interface Props {
 }
 export function AddConsumption({ consumptionAdded, selectedDate }: Props) {
   const today = isToday(selectedDate);
-  const { glassSize, unit } = usePreferencesContext();
-  const [loading, setLoading] = useState(false);
+  const { glassSize, bottleSize, unit } = usePreferencesContext();
+  const [loadingGlass, setLoadingGlass] = useState(false);
+  const [loadingBottle, setLoadingBottle] = useState(false);
 
-  async function handleAddConsumption() {
-    setLoading(true);
+  async function handleAddGlassConsumption() {
+    setLoadingGlass(true);
     await dbService.addConsumption({
       formattedDate: format(new Date(), "dd/MM/yyyy"),
       quantity: glassSize,
@@ -24,22 +25,54 @@ export function AddConsumption({ consumptionAdded, selectedDate }: Props) {
       origin: "smartphone"
     });
     consumptionAdded();
-    setLoading(false);
+    setLoadingGlass(false);
+  }
+
+  async function handleAddBottleConsumption() {
+    setLoadingBottle(true);
+    await dbService.addConsumption({
+      formattedDate: format(new Date(), "dd/MM/yyyy"),
+      quantity: bottleSize,
+      registerType: "bottle",
+      origin: "smartphone"
+    });
+    consumptionAdded();
+    setLoadingBottle(false);
   }
 
   return (
-    <View className="w-full items-center">
+    <View className="w-full flex-row gap-10 items-center justify-center">
       <TouchableOpacity
         className="w-40 h-14 rounded-full bg-accent-light items-center justify-center disabled:opacity-50"
-        onPress={handleAddConsumption}
-        disabled={loading || !today}
+        onPress={handleAddGlassConsumption}
+        disabled={loadingGlass || !today}
       >
-        {loading ? (
+        {loadingGlass ? (
           <ActivityIndicator size={20} color="white" />
         ) : (
-          <Text className="font-bold text-white">
-            +{glassSize} {unit}
-          </Text>
+          <View className="flex-row items-center gap-2">
+            <Icon name="glass" color="white" />
+            <Text className="font-bold text-white">
+              +{glassSize} {unit}
+            </Text>
+          </View>
+        )}
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        className="w-40 h-14 rounded-full bg-accent-light items-center justify-center disabled:opacity-50"
+        onPress={handleAddBottleConsumption}
+        disabled={loadingBottle || !today}
+      >
+        {loadingBottle ? (
+          <ActivityIndicator size={20} color="white" />
+        ) : (
+          <View className="flex-row items-center gap-2">
+            <Icon name="glass" color="white" />
+            <Text className="font-bold text-white">
+              +{bottleSize} {unit}
+            </Text>
+          </View>
         )}
       </TouchableOpacity>
     </View>
