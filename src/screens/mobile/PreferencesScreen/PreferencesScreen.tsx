b5e6183724b73f88eab9e1911dfebabdbb7b125c
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-07 13:43:19 -0300

    feat: add change goal and description text

--- src/screens/mobile/PreferencesScreen/PreferencesScreen.tsx
+++ src/screens/mobile/PreferencesScreen/PreferencesScreen.tsx
@@ -1,15 +1,19 @@
 import { View } from "react-native";
-import { Screen } from "@components";
+import { Screen, Text } from "@components";
 
 import { ChangeGlassSize } from "./components/ChangeGlassSize";
 import { ChangeBottleSize } from "./components/ChangeBottleSize";
+import { ChangeGoal } from "./components/ChangeGoal";
 
 export function PreferencesScreen() { 
   return (
     <Screen title="Preferências" showBackButton>
-      <View className="flex-1 w-full h-full items-center justify-center gap-10">
+      <View className="flex-1 w-full h-full items-center justify-center gap-10 px-5">
+        <Text className="text-secondary-text-light dark:text-secondary-text-dark text-center">
+          Aqui você pode personalizar sua experiência ajustando como deseja acompanhar sua hidratação diária. Defina sua meta de consumo de água, escolha o tamanho do copo que você costuma usar e configure o tamanho da garrafa para facilitar seus registros.
+        </Text>
+        <ChangeGoal />
         <ChangeGlassSize />
-
         <ChangeBottleSize />
       </View>
     </Screen>
---
+++ src/screens/mobile/PreferencesScreen/components/ChangeGoal.tsx
@@ -0,0 +1,30 @@
+import { View } from "react-native";
+
+import { Text } from "@components";
+import { NumberSelector } from "./NumberSelector";
+import { usePreferencesContext } from "@hooks";
+
+export function ChangeGoal() {
+  const { goal, changePreference } = usePreferencesContext();
+
+  function handleChangeGoal(type: "minus" | "plus") {
+    changePreference({
+      type: "goal",
+      value: type === "minus" ? goal - 50 : goal + 50
+    })
+  }
+
+  return (
+    <View className="items-center gap-1">
+      <Text className="text-sm text-secondary-text-light dark:text-secondary-text-dark">
+        Meta diária
+      </Text>
+
+      <NumberSelector
+        value={goal}
+        handleMinus={() => handleChangeGoal("minus")}
+        handlePlus={() => handleChangeGoal("plus")}
+      />
+    </View>
+  )
+}
